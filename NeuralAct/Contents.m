893312df2a57b21071832fe2f6dee88ddd151ab0
mariana <M.PedrosoBranco@umcutrecht.nl>
2016-01-04 08:13:54 +0000

    jan2015
    git-svn-id: svn://ribsbot.umcutrecht.nl/juniper/trunk@91 06bb8ab4-6320-4abd-9ac8-7eb61d049d8b

--- NeuralAct/Contents.m
+++ NeuralAct/Contents.m
@@ -7,6 +7,3 @@
 %   projectElectrodes       - Project electrode grids onto the cortical model.
 %   recordBrain             - Capture NeuralAct graphical output into an avi file.
 %   viewBrain               - Display a cortical model and/or electrode grid locations using NeuralAct.
-%   NeuralAct_procedure.jpg     - The individual processing steps
-%   NeuralAct_AB.jpg            - Steps A and B of the procedure
-%   NeuralAct_examples.jpg      - Example figures produced using NeuralAct
