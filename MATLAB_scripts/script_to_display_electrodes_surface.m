318e1ede895f26e7a40d150321cdbc2357084421
mariana <M.PedrosoBranco@umcutrecht.nl>
2020-07-10 10:25:32 +0000

    git-svn-id: svn://ribsbot.umcutrecht.nl/juniper/trunk@1854 06bb8ab4-6320-4abd-9ac8-7eb61d049d8b

---
+++ MATLAB_scripts/script_to_display_electrodes_surface.m
@@ -0,0 +1,28 @@
+mypath  = './results/';   %path fo ALICE/results folder
+subject = 'zuil'; %subject code name
+hemi    = 'L';    %implanted hemisphere (L, R or L&R)
+
+% load electrodes on surface
+load([mypath subject '_' hemi '_projectedElectrodes_FreeSurfer_3dclust.mat']);
+
+%load cortex
+load([mypath subject '_' hemi '_cortex.mat']);
+
+%set brain transparency
+facealpha = 0.3; %1 for opaque, 0 for transparent
+
+%plot brain
+ctmr_gauss_plot(cortex,[0 0 0],0,facealpha);
+
+%add electrodes
+el_add(elecmatrix,'r',20);
+
+%add label
+label_add(elecmatrix);
+
+%set view
+loc_view(90, 0);  %for right view
+loc_view(-90, 0); %for left view
+
+%save figure
+saveas(gcf,[subject '_' hemi '_SETVIEW.png']);
